60005ad05f5bd826c4eddb1ab59308fd8d5aa8b5
Your Name <dfsilvat@gmail.com>
2023-02-26 17:37:04 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int a = 12;
+        int b = 13;
+        System.out.println(a + b);
+    }
+}
---
+++ src/br/com/view/PrimeiroPorgrama.java
@@ -0,0 +1,21 @@
+package br.com.view;
+
+import br.com.view.Model.Gato;
+
+public class PrimeiroPorgrama {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+
+/*        String saudacao = "Hello world! ";
+        int a = 2;
+        int b = 3;
+        System.out.println(saudacao + a + b);*/
+
+
+
+    }
+}
