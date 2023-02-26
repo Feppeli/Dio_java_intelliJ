2f7f49bd4434f2cdb7d4f4ecad89866acf4f9549
Your Name <dfsilvat@gmail.com>
2023-02-26 18:16:22 -0300

    add: class Livro

--- src/br/com/view/PrimeiroPorgrama.java
+++ src/br/com/view/PrimeiroPorgrama.java
@@ -2,12 +2,17 @@ package br.com.view;
 
 import br.com.view.Model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPorgrama {
     public static void main(String[] args) {
 
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro1 = new Livro("abc", 300);
+
+        System.out.println(livro1);
 
 
 /*        String saudacao = "Hello world! ";
@@ -19,3 +24,56 @@ public class PrimeiroPorgrama {
 
     }
 }
+
+
+class Livro{
+
+    private String nome;
+    private Integer numPaginas;
+
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(nome, livro.nome) && Objects.equals(numPaginas, livro.numPaginas);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, numPaginas);
+    }
+}
+
